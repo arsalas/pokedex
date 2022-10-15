16518658e9e8b4d5ec434839d025f4939bc7b0d1
Alberto Ramirez <57710147+arsalas@users.noreply.github.com>
2022-10-15 13:35:46 +0200

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const routes = [
     },
 
 ]
-
+console.log(import.meta.env.BASE_URL)
 // 3. Create the router instance and pass the `routes` option
 // You can pass in additional options here, but let's
 // keep it simple for now.
